9a7bcf5899e1e01926b0b638d8a60a66b628f800
Jonathan Neal <jonathantneal@hotmail.com>
2014-05-21 09:56:21 -0700

    Configurations

---
+++ bower.js
@@ -0,0 +1,27 @@
+{
+	"name": "altclass",
+	"description": "Responsive classnames by element breakpoints",
+	"version": "0.0.1",
+	"main": ["altclass.js", "altclass.legacy.js"],
+	"license": "CC0-1.0",
+	"ignore": [
+		".editorconfig",
+		".jshintrc",
+		"bower.json",
+		"demo.html",
+		"LICENSE.md",
+		"README.md"
+	],
+	"keywords": [
+		"responsive",
+		"element",
+		"prototype",
+		"altclass",
+		"classname"
+	],
+	"homepage": "https://github.com/chapmanu/altclass",
+	"repository": {
+		"type": "git",
+		"url": "git://github.com/chapmanu/altclass.git"
+	}
+}
